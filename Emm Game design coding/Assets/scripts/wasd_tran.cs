62c2f2c5f3e66e5f2918836953fc7ee9b66b51f2
Em4X99 <158608018+Em4X99@users.noreply.github.com>
2024-09-12 17:09:50 -0400

    wasd 2

--- Emm Game design coding/Assets/scripts/wasd_tran.cs
+++ Emm Game design coding/Assets/scripts/wasd_tran.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class wasd_tran : MonoBehaviour
 {
+    //Variables
+    public float speed = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,36 @@ public class wasd_tran : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //find the position of the object every frame and make a vector
+        Vector2 pos = transform.position;
+
+        //If i press right arrow, move the position to the right
+        if (Input.GetKey(KeyCode.RightArrow)) 
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+
+       
+
+        //If i press left arrow, move the position to the left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+        //If i press up arrow, move the position to the up
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+
+        //If i press down arrow, move the position to the down
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+
+        //MOVE!
+        transform.position = pos;
     }
 }
